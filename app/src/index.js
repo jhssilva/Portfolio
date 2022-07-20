e15e7d3fe387828b03629d6fd87b314afe6f1b03
Hugo Silva <jhssilva96@gmail.com>
2022-07-20 12:31:56 +0100

    Firebase set.

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,28 @@ import "./index.css";
 import App from "./App";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPZquHNGssZLVB-qcEiAGde8XCmMkejvo",
+  authDomain: "hugosilvaportfolio.firebaseapp.com",
+  projectId: "hugosilvaportfolio",
+  storageBucket: "hugosilvaportfolio.appspot.com",
+  messagingSenderId: "142252168291",
+  appId: "1:142252168291:web:58fdc83492f0cdcd4b5456",
+  measurementId: "G-TT0PHZ1HNY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
